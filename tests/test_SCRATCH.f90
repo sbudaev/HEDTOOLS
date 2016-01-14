5d76d68991e6129a0e01a71fad8aa84ffb1087f5
Sergey Budaev <sergey.budaev@uib.no>
2016-01-14 16:37:15 +0000

    Test file, use CSV_RECORD_APPEND with arrays using implied do loop and array slices
    git-svn-id: https://tegsvn.uib.no/svn/tegsvn/branches/budaev/HEDTOOLS@259 ad98353e-09f0-4531-9f56-ca5884d0cf98

--- tests/test_SCRATCH.f90
+++ tests/test_SCRATCH.f90
@@ -14,32 +14,17 @@ program TEST_SCRATCH
   use CSV_IO
 
   character (len=255) :: REC
-  character (len=4) :: XXX = "1234"
-  integer, dimension(6) :: IARR = [1,2,3,4,5,6]
+  integer :: i
   real, dimension(6) :: RARR = [0.1,0.2,0.3,0.4,0.5,0.6]
-  
+
   REC=""
+  print *, ">>", trim(REC), "<<", (RARR(i),i=1,4)
+
+  i=5
+  call CSV_RECORD_APPEND( REC, (/(RARR(i), i=1,6)/) )
   print *, ">>", trim(REC), "<<"
-  
-  call CSV_RECORD_APPEND (REC, "one")
-  print *, ">>", trim(REC), "<<"
-  
-  call CSV_RECORD_APPEND (REC, "two", "three", "TTT", "ZZZ_1", "MMM", XXX)  
-  print *, ">>", trim(REC), "<<"
-  
-  call CSV_RECORD_APPEND(REC, 1.2, 1.3)
-  print *, ">>", trim(REC), "<<"
-  
-  call CSV_RECORD_APPEND(REC, IARR)
-  print *, ">>", trim(REC), "<<"
-  
-  call CSV_RECORD_APPEND(REC, RARR)
-  print *, ">>", trim(REC), "<<"
-  
-  call CSV_RECORD_APPEND(REC, ["A","B","C"])
-  print *, ">>", trim(REC), "<<"
-  
-  call CSV_RECORD_APPEND(REC, [102.1,102.2,109.])
+
+  call CSV_RECORD_APPEND( REC, RARR(1:4) )
   print *, ">>", trim(REC), "<<"
 
 end program TEST_SCRATCH
