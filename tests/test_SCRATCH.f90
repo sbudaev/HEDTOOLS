ea1407eb9721f9fb8e5e53b5a42fcae7c3fa610f
Sergey Budaev <sergey.budaev@uib.no>
2016-02-04 15:53:35 +0000

    Added various array constructors to CSV_RECORD_APPEND test program
    git-svn-id: https://tegsvn.uib.no/svn/tegsvn/branches/budaev/HEDTOOLS@280 ad98353e-09f0-4531-9f56-ca5884d0cf98

--- tests/test_SCRATCH.f90
+++ tests/test_SCRATCH.f90
@@ -13,18 +13,50 @@ program TEST_SCRATCH
   use BASE_UTILS
   use CSV_IO
 
-  character (len=255) :: REC
+  character (len=25500) :: REC
   integer :: i
   real, dimension(6) :: RARR = [0.1,0.2,0.3,0.4,0.5,0.6]
+  character (len=4), dimension(6) :: STARR=["a1","a2","a3","a4","a5","a6"]
 
   REC=""
   print *, ">>", trim(REC), "<<", (RARR(i),i=1,4)
 
   i=5
-  call CSV_RECORD_APPEND( REC, (/(RARR(i), i=1,6)/) )
+  ! using old Fortran arbitrary array constructor
+  call CSV_RECORD_APPEND( REC, (/(RARR(i), i=1,6)/) )  
   print *, ">>", trim(REC), "<<"
 
+  REC=""
+  ! Using array slices
   call CSV_RECORD_APPEND( REC, RARR(1:4) )
   print *, ">>", trim(REC), "<<"
+  
+  REC=""
+  ! using new fortran array constructor
+  call CSV_RECORD_APPEND( REC, [(RARR(i), i=1,4), 200.3, 14.077 ] )
+  print *, ">>", trim(REC), "<<"
+  
+  REC=""
+  ! using new fortran array constructor
+  call CSV_RECORD_APPEND( REC, [(i,i=1,10)] )
+  print *, ">>", trim(REC), "<<"
+  
+  REC=""
+  ! using old fortran array constructor
+  call CSV_RECORD_APPEND( REC, (/(i,i=100,1000,100)/) )
+  print *, ">>", trim(REC), "<<"
+  
+  REC=""
+  ! using string array
+  call CSV_RECORD_APPEND( REC, STARR )
+  print *, ">>", trim(REC), "<<"
+    
+  REC=""
+  ! using fortran array constructor to create strings
+  ! Note that implicit constructor sets the constructed string length 
+  ! at first invocation. So, set maximum length first (7 rather than 5)
+  call CSV_RECORD_APPEND( REC, [ "VAR_001", ("VAR_" // TOSTR(i),i=2,100) ] )
+  print *, ">>", trim(REC), "<<"
+  
 
 end program TEST_SCRATCH
