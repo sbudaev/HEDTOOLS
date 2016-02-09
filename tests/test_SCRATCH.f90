0ffbc9a65443b00967d8a87c154997bf89639c3d
Sergey Budaev <sergey.budaev@uib.no>
2016-02-09 04:26:04 +0000

    Started adding short documentation in asciidoc format (.adoc)
    git-svn-id: https://tegsvn.uib.no/svn/tegsvn/branches/budaev/HEDTOOLS@310 ad98353e-09f0-4531-9f56-ca5884d0cf98

--- tests/test_SCRATCH.f90
+++ tests/test_SCRATCH.f90
@@ -23,47 +23,70 @@ program TEST_SCRATCH
 
   i=5
   ! using old Fortran arbitrary array constructor
-  call CSV_RECORD_APPEND( REC, (/(RARR(i), i=1,6)/) )  
+  call CSV_RECORD_APPEND( REC, (/(RARR(i), i=1,6)/) )
   print *, ">>", trim(REC), "<<"
 
   REC=""
   ! Using array slices
   call CSV_RECORD_APPEND( REC, RARR(1:4) )
   print *, ">>", trim(REC), "<<"
-  
+
   REC=""
   ! using new fortran array constructor
   call CSV_RECORD_APPEND( REC, [(RARR(i), i=1,4), 200.3, 14.077 ] )
   print *, ">>", trim(REC), "<<"
-  
+
   REC=""
   ! using new fortran array constructor
   call CSV_RECORD_APPEND( REC, [(i,i=1,10)] )
   print *, ">>", trim(REC), "<<"
-  
+
   REC=""
   ! using old fortran array constructor
   call CSV_RECORD_APPEND( REC, (/(i,i=100,1000,100)/) )
   print *, ">>", trim(REC), "<<"
-  
+
   REC=""
   ! using string array
   call CSV_RECORD_APPEND( REC, STARR )
   print *, ">>", trim(REC), "<<"
-    
+
   REC=""
   ! using fortran array constructor to create strings
-  ! Note that implicit constructor sets the constructed string length 
+  ! Note that implicit constructor sets the constructed string length
   ! at first invocation. So, set maximum length first (7 rather than 5)
   call CSV_RECORD_APPEND( REC, [ "VAR_001", ("VAR_" // TOSTR(i),i=2,10), "VAR_00X but this long part is lost" ] )
   print *, ">>", trim(REC), "<<"
-  
-  ! TOSTR function can produce arbitrary number of leading zeroes 
+
+  ! TOSTR function can produce arbitrary number of leading zeroes
   print *, TOSTR(10, 100), TOSTR(-10, 100)
   print *, TOSTR(14, 10000), TOSTR(-14, 10000)
   print *, "File_" // TOSTR(10, 1000) // ".txt"
-  
-  REC = "XX" // TOSTR(12, 1000) // "XX"  
-  print *, ">>", trim(REC), "<<<"
+
+  REC = "XX" // TOSTR(12, 1000) // "XX"
+  print *, ">>", trim(REC), "<<"
+
+  print *, ">>", TOSTR(3.1415926,"(f4.2)"), "<<"
+
+  print *, ">>", TOSTR(3.1415926), "<<"
+
+  print *, ">>", TOSTR(3.1415926_8), "<<8"
+
+  print *, ">>", TOSTR(12,"(i4)"), "<<"
+
+  print *, ">>", TOSTR(10, 100), "<<", " ", ">>", TOSTR(10, 999), "<<"
+
+  print *, ">>", TOSTR(RARR), "<<"
+
+  print *, ">>", TOSTR(RARR,"(f4.2)"), "<<"
+
+  print *, ">>", TOSTR(RARR(1:4)), "<<"
+
+  print *, ">>", TOSTR( (/(RARR(i), i=1,4)/) ), "<<"
+
+  print *, ">>", TOSTR( [(RARR(i), i=1,4), 200.1, 400.5] ), "<<"
+
+  print *, ">>", TOSTR( [(RARR(i), i=1,4), 200.1, 400.5], "(f9.3)" ), "<<"
+
 
 end program TEST_SCRATCH
