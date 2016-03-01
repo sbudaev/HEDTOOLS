02f4de364f1346ae59b4f7b1b9122fc668520617
Sergey Budaev <sergey.budaev@uib.no>
2016-03-01 11:07:41 +0000

    delete scratch from tests, changes too often
    git-svn-id: https://tegsvn.uib.no/svn/tegsvn/branches/budaev/HEDTOOLS@356 ad98353e-09f0-4531-9f56-ca5884d0cf98

--- tests/test_SCRATCH.f90
+++ tests/test_SCRATCH.f90
@@ -13,90 +13,32 @@ program TEST_SCRATCH
   use BASE_UTILS
   use CSV_IO
 
-  character (len=255) :: REC
-  integer :: i
-  logical :: flag
-  real, dimension(6) :: RARR = [0.1,0.2,0.3,0.4,0.5,0.6]
-  character (len=4), dimension(6) :: STARR=["a1","a2","a3","a4","a5","a6"]
+  character (len=255), dimension(1:10) :: NAMES = ["aav01","aav02","aav03",&
+                        "aav04","aav05","aav06","aav07","aav08","aav09","aav10"]
+  !real (kind=4), dimension(1:100,1:20 ) :: MATRIX
+  real, dimension(1:100,1:300 ) :: MATRIX
+  !character (len=255), dimension(1:100,1:35 ) :: MATRIX
+  !integer, dimension(1:10) :: ARRAY
+  character (len=10), dimension(1:40)  :: NOUT
+  logical :: flag = .TRUE.
 
-  REC=""
-  print *, ">>", trim(REC), "<<", (RARR(i),i=1,4)
+!  call CSV_COLNAMES(NAMES,NOUT)
 
-  i=5
-  ! using old Fortran arbitrary array constructor
-  call CSV_RECORD_APPEND( REC, (/(RARR(i), i=1,6)/) )
-  print *, ">>", trim(REC), "<<"
+!  print *, NAMES
+!  print *, NOUT
 
-  REC=""
-  ! Using array slices
-  call CSV_RECORD_APPEND( REC, RARR(1:4) )
-  print *, ">>", trim(REC), "<<"
+  MATRIX=121.23456
+  !MATRIX="AAA BBB CCC DDD"
+  !ARRAY=149
 
-  REC=""
-  ! using new fortran array constructor
-  call CSV_RECORD_APPEND( REC, [(RARR(i), i=1,4), 200.3, 14.077 ] )
-  print *, ">>", trim(REC), "<<"
+  NAMES=[("a" // TOSTR(i,10),i=1,10)]
 
-  REC=""
-  ! using new fortran array constructor
-  call CSV_RECORD_APPEND( REC, [(i,i=1,10)] )
-  print *, ">>", trim(REC), "<<"
-
-  REC=""
-  ! using old fortran array constructor
-  call CSV_RECORD_APPEND( REC, (/(i,i=100,1000,100)/) )
-  print *, ">>", trim(REC), "<<"
-
-  REC=""
-  ! using string array
-  call CSV_RECORD_APPEND( REC, STARR )
-  print *, ">>", trim(REC), "<<"
-
-  REC=""
-  ! using fortran array constructor to create strings
-  ! Note that implicit constructor sets the constructed string length
-  ! at first invocation. So, set maximum length first (7 rather than 5)
-  call CSV_RECORD_APPEND( REC, [ "VAR_001", ("VAR_" // TOSTR(i),i=2,10) ] )
-  print *, ">>", trim(REC), "<<"
-
-  ! TOSTR function can produce arbitrary number of leading zeroes
-  print *, TOSTR(10, 100), TOSTR(-10, 100)
-  print *, TOSTR(14, 10000), TOSTR(-14, 10000)
-  print *, "File_" // TOSTR(10, 1000) // ".txt"
-
-  REC = "XX" // TOSTR(12, 1000) // "XX"
-  print *, ">>", trim(REC), "<<"
-
-  print *, ">>", TOSTR(3.1415926,"(f4.2)"), "<<"
-
-  print *, ">>", TOSTR(3.1415926), "<<"
-
-  print *, ">>", TOSTR(3.1415926_8), "<<8"
-
-  print *, ">>", TOSTR(12,"(i4)"), "<<"
-
-  print *, ">>", TOSTR(10, 100), "<<", " ", ">>", TOSTR(10, 999), "<<"
-
-  print *, ">>", TOSTR(RARR), "<<"
-
-  print *, ">>", TOSTR(RARR,"(f4.2)"), "<<"
-
-  print *, ">>", TOSTR(RARR(1:4)), "<<"
-
-  print *, ">>", TOSTR( (/(RARR(i), i=1,4)/) ), "<<"
-
-  print *, ">>", TOSTR( [(RARR(i), i=1,4), 200.1, 400.5] ), "<<"
-
-  print *, ">>", TOSTR( [(RARR(i), i=1,4), 200.1, 400.5], "(f9.3)" ), "<<"
-
-  call CSV_ARRAY_WRITE([("XXX OOO 111 444",i=1,10000)], "zzz_vert.csv", .TRUE., flag)
+  call CSV_MATRIX_WRITE(matrix=MATRIX, colnames=NAMES, csv_file_name="zzz1.csv", csv_file_status=flag)
   print *, flag
 
-  call CSV_ARRAY_WRITE([("XXX 000 111 222",i=1,1000)], "zzz_hor.csv", .FALSE., flag)
+  call CSV_MATRIX_WRITE(matrix=MATRIX, csv_file_name="zzz2.csv", csv_file_status=flag)
   print *, flag
 
-  print *, CSV_FILE_LINES_COUNT("zzz_vert.csv", flag)
-  print *,flag
 
 
 end program TEST_SCRATCH
