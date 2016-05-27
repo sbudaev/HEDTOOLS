b8e8cf6c95dbec57d3ed833bb990cd9c50e4e146
Sergey Budaev <sergey.budaev@uib.no>
2016-05-27 08:52:02 +0000

    Implemented RNORM function in mod RANDOM for normal distribution PRNG
    git-svn-id: https://tegsvn.uib.no/svn/tegsvn/branches/budaev/HEDTOOLS@661 ad98353e-09f0-4531-9f56-ca5884d0cf98

--- BASE_RANDOM.f90
+++ BASE_RANDOM.f90
@@ -155,9 +155,10 @@ contains
   end function
 
   function get_free_funit (file_status, max_funit) result (file_unit)
-  !*******************************************************************************
+  !*****************************************************************************
   ! GET_FREE_FUNIT
-  ! PURPOSE: returns the first free Fortran unit number (search in 1 to MAX_UNIT).
+  ! PURPOSE: returns the first free Fortran unit number (search in 1 to
+  !     MAX_UNIT).
   ! RETURNS:
   !    Integer unit number
   ! CALL PARAMETERS:
@@ -166,7 +167,7 @@ contains
   !
   ! Author: John Burkardt : This code is distributed under the GNU LGPL license.
   ! Modified by Sergey Budaev
-  !*******************************************************************************
+  !*****************************************************************************
 
     use, intrinsic :: ISO_FORTRAN_ENV     ! Provides system-wide scalar constants
                                           ! INPUT_UNIT OUTPUT_UNIT ERROR_UNIT
@@ -278,5 +279,58 @@ function RAND_I(a, b) result (randint)
 end function RAND_I
 
 !-------------------------------------------------------------------------------
+
+function RNORM() RESULT(fn_val)
+!*******************************************************************************
+! RNORM
+! PURPOSE: Returns a normally distributed pseudo-random number with zero mean
+!          and unit variance.
+! NOTES:   Adapted from the following Fortran 77 code
+!          ALGORITHM 712, COLLECTED ALGORITHMS FROM ACM.
+!          THIS WORK PUBLISHED IN TRANSACTIONS ON MATHEMATICAL SOFTWARE,
+!          VOL. 18, NO. 4, DECEMBER, 1992, PP. 434-435.
+!  The function RNORM() returns a normally distributed pseudo-random
+!  number with zero mean and unit variance.
+!  The algorithm uses the ratio of uniforms method of A.J. Kinderman
+!  and J.F. Monahan augmented with quadratic bounding curves.
+!  CODE SOURCE: http://www.netlib.org/ (random.f90)
+!*******************************************************************************
+
+REAL :: fn_val
+
+!     Local variables
+REAL     :: s = 0.449871, t = -0.386595, a = 0.19600, b = 0.25472,           &
+            r1 = 0.27597, r2 = 0.27846, u, v, x, y, q
+
+REAL     :: half = 0.5 ! moved here from header of the original module
+
+!     Generate P = (u,v) uniform in rectangle enclosing acceptance region
+
+DO
+  CALL random_number(u)
+  CALL random_number(v)
+  v = 1.7156 * (v - half)
+
+!     Evaluate the quadratic form
+  x = u - s
+  y = ABS(v) - t
+  q = x**2 + y*(a*y - b*x)
+
+!     Accept P if inside inner ellipse
+  IF (q < r1) EXIT
+!     Reject P if outside outer ellipse
+  IF (q > r2) CYCLE
+!     Reject P if outside acceptance region
+  IF (v**2 < -4.0*LOG(u)*u**2) EXIT
+END DO
+
+!     Return ratio of P's coordinates as the normal deviate
+fn_val = v/u
+RETURN
+
+end function RNORM
+
+!-------------------------------------------------------------------------------
+
 
 end module BASE_RANDOM ! <EOF>
