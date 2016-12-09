ea7312770c88969c744bbd79105d16fed6099f98
Sergey Budaev <sergey.budaev@uib.no>
2016-12-09 18:44:41 +0000

    first working version of (CSV_MATRIX_READ_R4)
    git-svn-id: https://tegsvn.uib.no/svn/tegsvn/branches/budaev/HEDTOOLS@2171 ad98353e-09f0-4531-9f56-ca5884d0cf98

--- BASE_CSV_IO.f90
+++ BASE_CSV_IO.f90
@@ -38,7 +38,7 @@ module CSV_IO
 ! Author: Sergey Budaev, based on csv_io functions by John Burkardt
 !*******************************************************************************
 
-!use BASE_STRINGS, only : VALUE
+use BASE_STRINGS, only : VALUE, PARSE, SPLIT, COMPACT, IS_NUMERIC
 
 implicit none
 
@@ -202,9 +202,7 @@ end interface CSV_GUESS_RECORD_LENGTH
 
 ! They are identical in CSV_IO and BASE_UTILS. Private here to avoid possible
 ! name conflicts, do we need them outside?
-private ::  I4_WIDTH, I4_LOG_10, STR_ITOA_LZ, STR_ITOA, CLEANUP, PARSE,       &
-            SPLIT, REMOVEBKSL, COMPACT
-
+private ::  I4_WIDTH, I4_LOG_10, STR_ITOA_LZ, STR_ITOA, CLEANUP
 
 ! This wrapper DEBUG LOG is used only for debugging this module. It may
 ! not or may use the module LOGGER, if not, (normally)  it can be used as a
@@ -906,14 +904,15 @@ end subroutine CSV_FILE_HEADER_WRITE_T
 
 !-------------------------------------------------------------------------------
 
-function CSV_FILE_LINES_COUNT (csv_file_name, csv_file_status) &
+function CSV_FILE_LINES_COUNT (csv_file_name, numeric_only, csv_file_status) &
         result (line_num)
 !*******************************************************************************
 ! CSV_FILE_LINE_COUNT
 ! PURPOSE: counts the number of lines in a CSV file.
 ! CALL PARAMETERS:
 !    Optional character CSV_FILE_NAME, the name of the file.
-!    Optional integer LINE_NUM, the number of lines.
+!    Optional flag indicating whether to count only numeric lines (if .TRUE.)
+!        (numeric are only lines including digits, spaces, comma and colon);
 !    Optional Optional logical CSV_FILE_STATUS, .TRUE. if no errors
 ! USES: GET_FREE_FUNIT from the same module
 ! NOTE: This routine does not try to distinguish the possible header line,
@@ -929,22 +928,31 @@ function CSV_FILE_LINES_COUNT (csv_file_name, csv_file_status) &
   integer :: line_num
 
   ! Calling parameters
-  character (len=*), intent(in) :: csv_file_name
+  character (len=*), intent(in)  :: csv_file_name
+  logical, optional, intent(in)  :: numeric_only
   logical, optional, intent(out) :: csv_file_status
 
   ! Local variables, copies of optionals
+  logical :: numeric_only_here
   logical :: csv_file_status_here
 
   ! Local variables
   integer :: ierror
   integer :: input_status
   integer :: input_unit
-  character (len=2) :: line   ! We only need to count rows here
+  !character (len=2) :: line   ! We only need to count rows here
+  character (len=:), allocatable :: line
 
   ! Subroutine name for DEBUG LOGGER
   character (len=*), parameter :: PROCNAME = "CSV_FILE_LINES_COUNT"
 
   !-----------------------------------------------------------------------------
+
+  if(present(numeric_only)) then
+    numeric_only_here = numeric_only
+  else
+    numeric_only_here = .FALSE.
+  end if
 
   ! May need initialise logical on some platforms/compilers
   if (present(csv_file_status)) csv_file_status = .TRUE.
@@ -968,13 +976,12 @@ function CSV_FILE_LINES_COUNT (csv_file_name, csv_file_status) &
 
   line_num = 0
 
-  do
-    read (input_unit, '(a2)', iostat = input_status) line
-    if (input_status /= 0) then
-      ierror=line_num
-      exit  ! leave the do loop here
+  do while ( READLINE(input_unit, line, .TRUE.) )
+    if (numeric_only_here) then
+      if (IS_NUMERIC(line, .TRUE.)) line_num=line_num+1
+    else
+      line_num = line_num + 1
     end if
-    line_num=line_num + 1
   end do
 
   close (unit=input_unit)
@@ -3980,191 +3987,6 @@ end function CLEANUP
 !       *private* to avoid name conflicts with BASE_STRINGS!
 !-------------------------------------------------------------------------------
 
-subroutine PARSE(str,delims,args,nargs)
-
-! Parses the string 'str' into arguments args(1), ..., args(nargs) based on
-! the delimiters contained in the string 'delims'. Preceding a delimiter in
-! 'str' by a backslash (\) makes this particular instance not a delimiter.
-! The integer output variable nargs contains the number of arguments found.
-
-character(len=*) :: str,delims
-character(len=len_trim(str)) :: strsav
-character(len=*),dimension(:) :: args
-
-integer :: nargs, i, k, lenstr, na
-
-strsav=str
-call compact(str)
-na=size(args)
-do i=1,na
-  args(i)=' '
-end do
-nargs=0
-lenstr=len_trim(str)
-if(lenstr==0) return
-k=0
-
-do
-   if(len_trim(str) == 0) exit
-   nargs=nargs+1
-   call split(str,delims,args(nargs))
-   call removebksl(args(nargs))
-end do
-str=strsav
-
-end subroutine PARSE
-
-subroutine SPLIT(str,delims,before,sep)
-
-! Routine finds the first instance of a character from 'delims' in the
-! the string 'str'. The characters before the found delimiter are
-! output in 'before'. The characters after the found delimiter are
-! output in 'str'. The optional output character 'sep' contains the
-! found delimiter. A delimiter in 'str' is treated like an ordinary
-! character if it is preceded by a backslash (\). If the backslash
-! character is desired in 'str', then precede it with another backslash.
-
-character(len=*) :: str,delims,before
-character,optional :: sep
-logical :: pres
-character :: ch,cha
-
-integer :: i, k, ibsl, ipos, iposa, lenstr
-
-pres=present(sep)
-str=adjustl(str)
-call compact(str)
-lenstr=len_trim(str)
-if(lenstr == 0) return        ! string str is empty
-k=0
-ibsl=0                        ! backslash initially inactive
-before=' '
-do i=1,lenstr
-   ch=str(i:i)
-   if(ibsl == 1) then          ! backslash active
-      k=k+1
-      before(k:k)=ch
-      ibsl=0
-      cycle
-   end if
-   if(ch == '\') then          ! backslash with backslash inactive
-      k=k+1
-      before(k:k)=ch
-      ibsl=1
-      cycle
-   end if
-   ipos=index(delims,ch)
-   if(ipos == 0) then          ! character is not a delimiter
-      k=k+1
-      before(k:k)=ch
-      cycle
-   end if
-   if(ch /= ' ') then          ! character is a delimiter that is not a space
-      str=str(i+1:)
-      if(pres) sep=ch
-      exit
-   end if
-   cha=str(i+1:i+1)            ! character is a space delimiter
-   iposa=index(delims,cha)
-   if(iposa > 0) then          ! next character is a delimiter
-      str=str(i+2:)
-      if(pres) sep=cha
-      exit
-   else
-      str=str(i+1:)
-      if(pres) sep=ch
-      exit
-   end if
-end do
-if(i >= lenstr) str=''
-str=adjustl(str)              ! remove initial spaces
-return
-
-end subroutine SPLIT
-
-subroutine REMOVEBKSL(str)
-
-! Removes backslash (\) characters. Double backslashes (\\) are replaced
-! by a single backslash.
-
-character(len=*):: str
-character(len=1):: ch
-character(len=len_trim(str))::outstr
-
-integer :: i, k, lenstr, ibsl
-
-str=adjustl(str)
-lenstr=len_trim(str)
-outstr=' '
-k=0
-ibsl=0                        ! backslash initially inactive
-
-do i=1,lenstr
-  ch=str(i:i)
-  if(ibsl == 1) then          ! backslash active
-   k=k+1
-   outstr(k:k)=ch
-   ibsl=0
-   cycle
-  end if
-  if(ch == '\') then          ! backslash with backslash inactive
-   ibsl=1
-   cycle
-  end if
-  k=k+1
-  outstr(k:k)=ch              ! non-backslash with backslash inactive
-end do
-
-str=adjustl(outstr)
-
-end subroutine REMOVEBKSL
-
-subroutine COMPACT(str)
-
-! Converts multiple spaces and tabs to single spaces; deletes control characters;
-! removes initial spaces.
-
-character(len=*):: str
-character(len=1):: ch
-character(len=len_trim(str)):: outstr
-
-integer i, k, ich, isp, lenstr
-
-str=adjustl(str)
-lenstr=len_trim(str)
-outstr=' '
-isp=0
-k=0
-
-do i=1,lenstr
-  ch=str(i:i)
-  ich=iachar(ch)
-
-  select case(ich)
-
-    case(9,32)     ! space or tab character
-      if(isp==0) then
-        k=k+1
-        outstr(k:k)=' '
-      end if
-      isp=1
-
-    case(33:)      ! not a space, quote, or control character
-      k=k+1
-      outstr(k:k)=ch
-      isp=0
-
-  end select
-
-end do
-
-str=adjustl(outstr)
-
-end subroutine COMPACT
-
-
-
-
 
 !-------------------------------------------------------------------------------
 
@@ -4233,7 +4055,7 @@ end function READLINE
 
 !-------------------------------------------------------------------------------
 
-function CSV_MATRIX_READ_R4 (csv_file_name, csv_file_status) result (matrix)
+function CSV_MATRIX_READ_R4 (csv_file_name, csv_file_status, missing_code) result (matrix)
 !*******************************************************************************
 ! CSV_MATRIX_READ_R4
 ! PURPOSE: Reads a matrix of real type from a CSV data file
@@ -4250,31 +4072,40 @@ function CSV_MATRIX_READ_R4 (csv_file_name, csv_file_status) result (matrix)
   real, dimension(:,:), allocatable :: matrix
   character (len=*), intent(in) :: csv_file_name
   logical, optional, intent(out) :: csv_file_status
+  real, optional, intent(in) :: missing_code
 
   ! Local variables, operations with files
   integer :: file_unit, error_iflag
   logical :: file_status
 
-  ! Local variables, operations with the data array.                            line_data_string
-  integer :: lines_infile, iline, jfield
+  ! Local variables, operations with the data array.
+  integer :: i, lines_infile, iline, icase, jfield, line_data_buff_length
   character(len=:), allocatable :: line_data_buff
   integer, parameter :: LEN_CSV_FIELD = 64 ! The length of a single field within a record
   integer, parameter :: MIN_FIELD = 2 ! Minimum length of a data field
   character(len=LEN_CSV_FIELD), dimension(:), allocatable  :: line_data_substrings
   integer :: line_data_nflds
   real, allocatable, dimension(:) :: matrix_row
+  real ::missing_code_here
+  real, parameter :: MISSING = -9999.0
+  logical :: not_a_data_row
   integer ::nvars
   integer ::ncases
 
   !> Delimiters for data fields:
   character(len=*), parameter :: SDELIM = " ,"
 
+
+  if(present(missing_code)) then
+    missing_code_here = missing_code
+  else
+    missing_code_here = MISSING
+  end if
+
+
   print *, csv_file_name
   file_status = .TRUE.
 
-
-  inquire(file=csv_file_name, size=nvars)
-  print *, nvars
 
   !line_data_buff=repeat(" ", 255)
   !line_data_buff="  "
@@ -4288,6 +4119,14 @@ function CSV_MATRIX_READ_R4 (csv_file_name, csv_file_status) result (matrix)
     return
   end if
 
+  ! Second, calculate the number of numeric-only lines in the file.
+  ncases = CSV_FILE_LINES_COUNT(  csv_file_name=csv_file_name,                &
+                                  numeric_only=.TRUE.,                        &
+                                  csv_file_status=file_status )
+  if (.NOT. file_status) then ! File opening error, try to close and exit
+    goto 1000
+  end if
+
   ! Then try to open the CSV file.
   call CSV_FILE_OPEN_READ (csv_file_name, file_unit, file_status)
   if (.NOT. file_status) then ! File opening error, try to close and exit
@@ -4296,21 +4135,69 @@ function CSV_MATRIX_READ_R4 (csv_file_name, csv_file_status) result (matrix)
 
 
   iline = 0
+  icase = 0
+  nvars = 0
   !do iline = 1, lines_infile
   do while ( READLINE(file_unit, line_data_buff, .TRUE.) )
+
     iline = iline + 1
 
-    allocate(line_data_substrings(len_trim(line_data_buff)/MIN_FIELD))
+    line_data_buff_length = len_trim(line_data_buff)
+
+    ! An approximate upper bound for the number of data fields in the
+    ! current record is len_trim(line_data_buff)/MIN_FIELD), so we can now
+    ! allocate the temporary array of substrings.
+    ! This is also the number of variables.
+    allocate(line_data_substrings(line_data_buff_length/MIN_FIELD))
 
     call PARSE ( line_data_buff, SDELIM, line_data_substrings, line_data_nflds )
 
+    ! We have to allocate the array of row values and initialise it for correct
+    ! processing by the VALUE subroutine.
     allocate(matrix_row(line_data_nflds))
+    matrix_row = MISSING
+
+    ! Empty lines are consideded non-data.
+    if ( line_data_buff_length>0 ) then
+      not_a_data_row = .FALSE.
+    else
+      not_a_data_row = .TRUE.
+    end if
 
     do jfield=1, line_data_nflds
-      !call VALUE(trim(line_data_substrings(jfield)), matrix_row(jfield), error_iflag)
+      !print *, "--->", jfield , trim(line_data_substrings(jfield)), IS_NUMERIC(trim(line_data_substrings(jfield)))
+
+      if (IS_NUMERIC(line_data_substrings(jfield))) then
+        call VALUE(trim(line_data_substrings(jfield)), matrix_row(jfield), error_iflag)
+      else
+        matrix_row(jfield) = missing_code_here
+        not_a_data_row = .TRUE.
+      end if
+
     end do
 
-    print *, "FF:", trim(line_data_substrings(1)), line_data_nflds, len_trim(line_data_buff)/MIN_FIELD
+    if (.not. not_a_data_row .and. line_data_buff_length>0 ) icase = icase + 1
+    ! If this is the first numeric case, we can assess the number of variables
+    ! and allocate the output data matrix.
+    if (icase == 1) then
+      nvars = line_data_nflds
+      allocate(matrix(nvars, ncases))
+    end if
+
+
+    !print *, "FF:", trim(line_data_substrings(1)), line_data_nflds, len_trim(line_data_buff)/MIN_FIELD
+    !print *, "FF:", iline, ncases, nvars, icase, matrix_row, line_data_nflds,  not_a_data_row
+    !print *, "FF:", iline, icase, ncases, nvars, matrix_row
+
+    !print *, ">,", [(i,i=1,nvars)]
+
+    if (.not. not_a_data_row) then
+      !print *, "ZZZ", iline, icase, matrix_row, line_data_buff_length
+      matrix(icase,1:nvars) = matrix_row(1:nvars)
+      print *, iline, icase, matrix(icase,:)
+    end if
+    !print *, iline, matrix(icase,:)
+
 
 
     deallocate(matrix_row)
@@ -4318,13 +4205,15 @@ function CSV_MATRIX_READ_R4 (csv_file_name, csv_file_status) result (matrix)
 
   end do
 
+  print *, "MMM",  matrix
+
 
 
   !Debug:
-  nvars = 100
-  ncases = 100
-  allocate(matrix(nvars,ncases))
-  matrix = 0.0
+  !nvars = 100
+  !ncases = 100
+  !allocate(matrix(nvars,ncases))
+  !matrix = 0.0
   print *, "XXX"
 
   if (present(csv_file_status)) csv_file_status = .TRUE.
